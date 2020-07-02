28d9ca55a6cedc748a474d482966b83772d89ec2
Kartik Singal <ka4tik@gmail.com>
2020-07-02 13:36:34 +0530

    encryptor decryptor working

--- native/examples/examples.cpp
+++ native/examples/examples.cpp
@@ -1,100 +1,332 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-#include "examples.h"
+#pragma once
 
+#include "seal/seal.h"
+#include <seal/randomgen.h>
+#include <seal/keygenerator.h>
+#include <memory>
+
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
 using namespace std;
 using namespace seal;
-
-int main()
+inline void print_parameters(std::shared_ptr<seal::SEALContext> context)
 {
-    cout << "Microsoft SEAL version: " << SEAL_VERSION << endl;
-    while (true)
+    // Verify parameters
+    if (!context)
     {
-        cout << "+---------------------------------------------------------+" << endl;
-        cout << "| The following examples should be executed while reading |" << endl;
-        cout << "| comments in associated files in native/examples/.       |" << endl;
-        cout << "+---------------------------------------------------------+" << endl;
-        cout << "| Examples                   | Source Files               |" << endl;
-        cout << "+----------------------------+----------------------------+" << endl;
-        cout << "| 1. BFV Basics              | 1_bfv_basics.cpp           |" << endl;
-        cout << "| 2. Encoders                | 2_encoders.cpp             |" << endl;
-        cout << "| 3. Levels                  | 3_levels.cpp               |" << endl;
-        cout << "| 4. CKKS Basics             | 4_ckks_basics.cpp          |" << endl;
-        cout << "| 5. Rotation                | 5_rotation.cpp             |" << endl;
-        cout << "| 6. Serialization           | 6_serialization.cpp        |" << endl;
-        cout << "| 7. Performance Test        | 7_performance.cpp          |" << endl;
-        cout << "+----------------------------+----------------------------+" << endl;
+        throw std::invalid_argument("context is not set");
+    }
+    auto &context_data = *context->key_context_data();
 
-        /*
-        Print how much memory we have allocated from the current memory pool.
-        By default the memory pool will be a static global pool and the
-        MemoryManager class can be used to change it. Most users should have
-        little or no reason to touch the memory allocation system.
-        */
-        size_t megabytes = MemoryManager::GetPool().alloc_byte_count() >> 20;
-        cout << "[" << setw(7) << right << megabytes << " MB] "
-             << "Total allocation from the memory pool" << endl;
+    /*
+    Which scheme are we using?
+    */
+    std::string scheme_name;
+    switch (context_data.parms().scheme())
+    {
+    case seal::scheme_type::BFV:
+        scheme_name = "BFV";
+        break;
+    case seal::scheme_type::CKKS:
+        scheme_name = "CKKS";
+        break;
+    default:
+        throw std::invalid_argument("unsupported scheme");
+    }
+    std::cout << "/" << std::endl;
+    std::cout << "| Encryption parameters :" << std::endl;
+    std::cout << "|   scheme: " << scheme_name << std::endl;
+    std::cout << "|   poly_modulus_degree: " << context_data.parms().poly_modulus_degree() << std::endl;
 
-        int selection = 0;
-        bool invalid = true;
-        do
-        {
-            cout << endl << "> Run example (1 ~ 7) or exit (0): ";
-            if (!(cin >> selection))
-            {
-                invalid = false;
-            }
-            else if (selection < 0 || selection > 7)
-            {
-                invalid = false;
-            }
-            else
-            {
-                invalid = true;
-            }
-            if (!invalid)
-            {
-                cout << "  [Beep~~] Invalid option: type 0 ~ 7" << endl;
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-        } while (!invalid);
+    /*
+    Print the size of the true (product) coefficient modulus.
+    */
+    std::cout << "|   coeff_modulus size: ";
+    std::cout << context_data.total_coeff_modulus_bit_count() << " (";
+    auto coeff_modulus = context_data.parms().coeff_modulus();
+    std::size_t coeff_modulus_size = coeff_modulus.size();
+    for (std::size_t i = 0; i < coeff_modulus_size - 1; i++)
+    {
+        std::cout << coeff_modulus[i].bit_count() << " + ";
+    }
+    std::cout << coeff_modulus.back().bit_count();
+    std::cout << ") bits" << std::endl;
 
-        switch (selection)
-        {
-        case 1:
-            example_bfv_basics();
-            break;
-
-        case 2:
-            example_encoders();
-            break;
-
-        case 3:
-            example_levels();
-            break;
-
-        case 4:
-            example_ckks_basics();
-            break;
-
-        case 5:
-            example_rotation();
-            break;
-
-        case 6:
-            example_serialization();
-            break;
-
-        case 7:
-            example_performance_test();
-            break;
-
-        case 0:
-            return 0;
-        }
+    /*
+    For the BFV scheme print the plain_modulus parameter.
+    */
+    if (context_data.parms().scheme() == seal::scheme_type::BFV)
+    {
+        std::cout << "|   plain_modulus: " << context_data.parms().plain_modulus().value() << std::endl;
     }
 
-    return 0;
+    std::cout << "\\" << std::endl;
+}
+/*
+Helper function: Print line number.
+*/
+inline void print_line(int line_number)
+{
+    std::cout << "Line " << std::setw(3) << line_number << " --> ";
+}
+template <typename T>
+inline void print_vector(std::vector<T> vec, std::size_t print_size = 4, int prec = 3)
+{
+    /*
+    Save the formatting information for std::cout.
+    */
+    std::ios old_fmt(nullptr);
+    old_fmt.copyfmt(std::cout);
+
+    std::size_t slot_count = vec.size();
+
+    std::cout << std::fixed << std::setprecision(prec);
+    std::cout << std::endl;
+    if (slot_count <= 2 * print_size)
+    {
+        std::cout << "    [";
+        for (std::size_t i = 0; i < slot_count; i++)
+        {
+            std::cout << " " << vec[i] << ((i != slot_count - 1) ? "," : " ]\n");
+        }
+    }
+    else
+    {
+        vec.resize(std::max(vec.size(), 2 * print_size));
+        std::cout << "    [";
+        for (std::size_t i = 0; i < print_size; i++)
+        {
+            std::cout << " " << vec[i] << ",";
+        }
+        if (vec.size() > 2 * print_size)
+        {
+            std::cout << " ...,";
+        }
+        for (std::size_t i = slot_count - print_size; i < slot_count; i++)
+        {
+            std::cout << " " << vec[i] << ((i != slot_count - 1) ? "," : " ]\n");
+        }
+    }
+    std::cout << std::endl;
+
+    /*
+    Restore the old std::cout formatting.
+    */
+    std::cout.copyfmt(old_fmt);
+}
+static std::string Encode(const std::string data)
+{
+    static constexpr char sEncodingTable[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                               'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
+
+    size_t in_len = data.size();
+    size_t out_len = 4 * ((in_len + 2) / 3);
+    std::string ret(out_len, '\0');
+    size_t i;
+    char *p = const_cast<char *>(ret.c_str());
+
+    for (i = 0; i < in_len - 2; i += 3)
+    {
+        *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+        *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int)(data[i + 1] & 0xF0) >> 4)];
+        *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2) | ((int)(data[i + 2] & 0xC0) >> 6)];
+        *p++ = sEncodingTable[data[i + 2] & 0x3F];
+    }
+    if (i < in_len)
+    {
+        *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+        if (i == (in_len - 1))
+        {
+            *p++ = sEncodingTable[((data[i] & 0x3) << 4)];
+            *p++ = '=';
+        }
+        else
+        {
+            *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int)(data[i + 1] & 0xF0) >> 4)];
+            *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2)];
+        }
+        *p++ = '=';
+    }
+
+    return ret;
+}
+
+static std::string Decode(const std::string &input, std::string &out)
+{
+    static constexpr unsigned char kDecodingTable[] = {
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63, 52, 53, 54, 55,
+        56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64, 64, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+        13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64, 64, 26, 27, 28, 29, 30, 31, 32,
+        33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+        64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+    };
+
+    size_t in_len = input.size();
+    if (in_len % 4 != 0)
+        return "Input data size is not a multiple of 4";
+
+    size_t out_len = in_len / 4 * 3;
+    if (input[in_len - 1] == '=')
+        out_len--;
+    if (input[in_len - 2] == '=')
+        out_len--;
+
+    out.resize(out_len);
+
+    for (size_t i = 0, j = 0; i < in_len;)
+    {
+        uint32_t a = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
+        uint32_t b = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
+        uint32_t c = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
+        uint32_t d = input[i] == '=' ? 0 & i++ : kDecodingTable[static_cast<int>(input[i++])];
+
+        uint32_t triple = (a << 3 * 6) + (b << 2 * 6) + (c << 1 * 6) + (d << 0 * 6);
+
+        if (j < out_len)
+            out[j++] = (triple >> 2 * 8) & 0xFF;
+        if (j < out_len)
+            out[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < out_len)
+            out[j++] = (triple >> 0 * 8) & 0xFF;
+    }
+
+    return "";
+}
+string encrypt(double num)
+{
+    random_seed_type secret_keyt = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    std::shared_ptr<UniformRandomGeneratorFactory> rg = make_shared<BlakePRNGFactory>(secret_keyt);
+    EncryptionParameters parms(scheme_type::CKKS);
+    size_t poly_modulus_degree = 8192;
+    parms.set_poly_modulus_degree(poly_modulus_degree);
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 40, 40, 40, 40, 40 }));
+
+    parms.set_random_generator(rg);
+    auto context = SEALContext::Create(parms, false);
+
+    stringstream public_ss;
+    KeyGenerator keygen(context);
+    auto secret_key = keygen.secret_key();
+    auto public_key = keygen.public_key();
+    auto relin_keys = keygen.relin_keys_local();
+
+    /*
+    We also set up an Encryptor, Evaluator, and Decryptor as usual.
+    */
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+    CKKSEncoder encoder(context);
+
+    size_t slot_count = encoder.slot_count();
+
+    vector<double> input{ num, 1.1, 2.2, 3.3 };
+    cout << "Input vector: " << endl;
+
+    Plaintext plain;
+    double scale = pow(2.0, 30);
+    print_line(__LINE__);
+    cout << "Encode input vector." << endl;
+    encoder.encode(input, scale, plain);
+
+    vector<double> output;
+
+    Ciphertext encrypted;
+    print_line(__LINE__);
+    cout << "Encrypt input vector, square, and relinearize." << endl;
+    encryptor.encrypt(plain, encrypted);
+    stringstream ss;
+    encrypted.save(ss);
+
+    return Encode(ss.str());
+}
+double decrypt(string e)
+{
+    string out;
+    e = Decode(e, out);
+    e = out;
+    random_seed_type secret_keyt = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    std::shared_ptr<UniformRandomGeneratorFactory> rg = make_shared<BlakePRNGFactory>(secret_keyt);
+    EncryptionParameters parms(scheme_type::CKKS);
+    size_t poly_modulus_degree = 8192;
+    parms.set_poly_modulus_degree(poly_modulus_degree);
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 40, 40, 40, 40, 40 }));
+
+    parms.set_random_generator(rg);
+    auto context = SEALContext::Create(parms, false);
+
+    stringstream public_ss;
+    KeyGenerator keygen(context);
+    auto secret_key = keygen.secret_key();
+    auto public_key = keygen.public_key();
+    auto relin_keys = keygen.relin_keys_local();
+
+    /*
+    We also set up an Encryptor, Evaluator, and Decryptor as usual.
+    */
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+    CKKSEncoder encoder(context);
+
+    size_t slot_count = encoder.slot_count();
+
+
+    Plaintext plain;
+  
+    vector<double> output;
+
+
+    Ciphertext encrypted;
+    stringstream ss = stringstream(e);
+    encrypted.load(context, ss);
+
+    print_line(__LINE__);
+    cout << "Decrypt and decode." << endl;
+    decryptor.decrypt(encrypted, plain);
+    encoder.decode(plain, output);
+   
+    return output[0];
+
+
+
+ }
+
+     int main()
+{
+
+      string e = encrypt(100);
+      cout << e << endl;
+      double d = decrypt(e);
+      cout << d << endl;
+         
+    
+  
+
 }
